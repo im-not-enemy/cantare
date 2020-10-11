f36010a4653cd150ecad675967ac197665ecac05
im-not-enemy <opkd0710@gmail.com>
2020-10-11 14:34:54 +0000

    [Menulist] delete機能追加

--- src/components/Parts/MenuCard.vue
+++ src/components/Parts/MenuCard.vue
@@ -2,11 +2,14 @@
     <div id="card">
         <div id="paper"></div>
         <div :class="data._id"></div>
+        <button class="delete" @click="remove">X</button>
     </div>
 </template>
 
 <script>
 import abcjs from 'abcjs'
+import axios from 'axios'
+import setting from '../../conf/setting'
 
 export default {
     props: ["data","width"],
@@ -25,16 +28,27 @@ export default {
         const data = document.getElementsByClassName(this.data._id)[0]
         data.appendChild(svg[0]) //再レンダリング
         paper.remove() //#paperは削除
+    },
+    methods: {
+        remove(){
+            axios.delete(`${setting.server}/menu/${this.data._id}`)
+        }
     }
 }
 </script>
 
 <style scoped>
 #card {
+    position: relative;
     border: solid 1px gray;
     margin-top: 1px;
     margin-bottom: 1px;
     width: 100%;
     background: lightcyan;
+}
+.delete {
+    position: absolute;
+    top: 0;
+    right: 0;
 }
 </style>
