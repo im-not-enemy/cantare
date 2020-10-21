5b1f806b5bf1c054a74f70cc8e5aa69c14fabbb4
im-not-enemy <opkd0710@gmail.com>
2020-10-21 13:28:14 +0000

    [app] レイアウト修正

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,14 +1,14 @@
 <template>
     <div id="app">
         <div class="header">
-            <button @click="this.show">三</button>
+            <button @click="show=!show">三</button>
         </div>
-        <div id="show-links" v-show="seen" v-touch:swipe.left="this.hide">
+        <div id="show-links" v-show="show" v-touch:swipe.left="hide">
             <router-link to="/studio">studio</router-link><br> 
             <router-link to="/menulist">menulist</router-link><br>
             <router-link to="/training">training</router-link><br> 
-            <button @click="this.enable" v-if="!(this.full)">full: enable</button>
-            <button @click="this.disable" v-if="this.full">full: disable</button>
+            <button @click="enable" v-if="!(full)">full: enable</button>
+            <button @click="disable" v-if="full">full: disable</button>
         </div>
         <router-view></router-view>
     </div>
@@ -19,16 +19,13 @@
 export default {
     data: function(){
         return {
-            seen: true,
+            show: true,
             full: false
         }
     },
     methods: {
         hide: function(){
-            this.seen = false
-        },
-        show: function(){
-            this.seen = true
+            this.show= false
         },
         enable(){
             document.getElementById('app').requestFullscreen()
@@ -52,10 +49,10 @@ export default {
     position: fixed;
     top: 0;
     left: 0;
+    padding: 45px 5px 5px 5px;
     height: 100vh;
     width: 200px;
-    z-index: 150;
-    padding: 5px;
+    z-index: 100;
 }
 #show-links * {
     margin: 5px;
@@ -78,7 +75,7 @@ router-view {
     width: 100%;
     margin: 0px;
     padding: 3px;
-    z-index: 100;
+    z-index: 150;
     display: flex;
 }
 </style>
